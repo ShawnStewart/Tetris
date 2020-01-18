3cc7c384c0507ec37c0bda0ae632d592e9ff749d
Shawn Stewart <shawn.stewart@quarkworks.co>
2020-01-17 21:34:30 -0600

    Move draw function into utils

--- Tetris/src/components/Canvas.js
+++ Tetris/src/components/Canvas.js
@@ -1,53 +1,21 @@
 import React, { useEffect, useRef } from 'react';
 
+import { BLOCK_SIZE } from '../constants';
+import { drawToCanvas } from '../utils';
+
 import './canvas.scss';
-import { BLOCK_SIZE, COLOR_MAP } from '../constants';
 
-const draw = ({ canvas, m }) => {
-    const ctx = canvas.getContext('2d');
-
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < m.length; i++) {
-        for (let j = 0; j < m[0].length; j++) {
-            // Draw square
-            ctx.fillStyle = COLOR_MAP[m[i][j]];
-            ctx.fillRect(
-                j * BLOCK_SIZE + 1,
-                i * BLOCK_SIZE + 1,
-                BLOCK_SIZE,
-                BLOCK_SIZE,
-            );
-
-            // Draw square border
-            if (m[i][j]) {
-                ctx.strokeStyle = '#000000';
-                ctx.beginPath();
-
-                ctx.moveTo(j * BLOCK_SIZE + 0.5, i * BLOCK_SIZE + 0.5);
-                ctx.lineTo(j * BLOCK_SIZE + 0.5, (i + 1) * BLOCK_SIZE + 0.5);
-                ctx.lineTo(
-                    (j + 1) * BLOCK_SIZE + 0.5,
-                    (i + 1) * BLOCK_SIZE + 0.5,
-                );
-                ctx.lineTo((j + 1) * BLOCK_SIZE + 0.5, i * BLOCK_SIZE + 0.5);
-                ctx.lineTo(j * BLOCK_SIZE + 0.5, i * BLOCK_SIZE + 0.5);
-
-                ctx.stroke();
-            }
-        }
-    }
-};
-
-const Canvas = ({ id, matrix, height, width }) => {
+const Canvas = ({ id, matrix, height, width, forwardRef }) => {
     const h = matrix && matrix.length ? matrix.length : height;
     const w =
         matrix && matrix.length && matrix[0].length ? matrix[0].length : width;
-    const canvasRef = useRef();
+    const canvasRef = forwardRef || useRef();
 
     useEffect(() => {
-        draw({ canvas: canvasRef.current, m: matrix });
+        const canvas = canvasRef.current;
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        drawToCanvas({ canvas: canvasRef.current, matrix });
     }, [matrix]);
 
     return (
--- Tetris/src/utils/index.js
+++ Tetris/src/utils/index.js
@@ -1,3 +1,34 @@
+import { BLOCK_SIZE, COLOR_MAP } from '../constants';
+
+export const drawToCanvas = ({ canvas, matrix: m, x = 0, y = 0 }) => {
+    const ctx = canvas.getContext('2d');
+
+    for (let i = 0; i < m.length; i++) {
+        for (let j = 0; j < m[0].length; j++) {
+            const posX = (j + x) * BLOCK_SIZE;
+            const posY = (i + y) * BLOCK_SIZE;
+
+            // Draw square
+            ctx.fillStyle = COLOR_MAP[m[i][j]];
+            ctx.fillRect(posX + 1, posY + 1, BLOCK_SIZE, BLOCK_SIZE);
+
+            // Draw square border
+            if (m[i][j]) {
+                ctx.strokeStyle = '#000000';
+                ctx.beginPath();
+
+                ctx.moveTo(posX + 0.5, posY + 0.5);
+                ctx.lineTo(posX + 0.5, posY + BLOCK_SIZE + 0.5);
+                ctx.lineTo(posX + BLOCK_SIZE + 0.5, posY + BLOCK_SIZE + 0.5);
+                ctx.lineTo(posX + BLOCK_SIZE + 0.5, posY + 0.5);
+                ctx.lineTo(posX + 0.5, posY + 0.5);
+
+                ctx.stroke();
+            }
+        }
+    }
+};
+
 export const rotateMatrix = ({ clockwise = true, matrix: m }) => {
     const n = m.length;
 
